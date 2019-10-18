877252fec0a0f922868a00c3e85ff83d76f3181f
DgbDiego <dgb.diego@gmail.com>
2019-10-18 13:46:31 -0300

    2 files modified

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -7,14 +7,14 @@ import { CartService } from '../cart.service';
   styleUrls: ['./shipping.component.css']
 })
 export class ShippingComponent implements OnInit {
-  shippingCost;
+  shippingCosts;
 
   constructor(
     private cartService: CartService
   ) { }
 
   ngOnInit() {
-    this.shippingCost = this.cartService.getShippingPrices();
+    this.shippingCosts = this.cartService.getShippingPrices();
   }
 
 }
